5ba5c8f783b2a86e6cd6336b8504dd0274458f64
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-11-10 14:03:25 +0100

    Remove id

--- wp-content/themes/foce-child/templates/oscar-section.php
+++ wp-content/themes/foce-child/templates/oscar-section.php
@@ -1,4 +1,4 @@
-<section id="oscars" class="oscars">
+<section class="oscars">
         <h3> Fleurs d’oranger & chats errants<br> est nominé aux Oscars Short Film Animated de 2022 ! </h3>
         <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/oscars_shorts.png'; ?>" alt="Logo des oscars">
 </section>
