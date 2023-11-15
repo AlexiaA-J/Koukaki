ecd769e3bb438a6672b16bdf09f8dffa74121990
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-11-15 15:30:27 +0100

    Modify parameters of swiper

--- wp-content/themes/foce-child/assets/js/script.js
+++ wp-content/themes/foce-child/assets/js/script.js
@@ -1,8 +1,29 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
+  pagination:'.swiper-pagination',
   effect: 'coverflow',
+  grabCursor:true,
+  slidesPerView: 3,
+  initialSlide: 0,
+  coverflowEffect: {
+    rotate:60,
+    stretch:-40,
+    depth:100,
+    modifier:1,
+    slideshadows:true,
+}
 });
 
-  
+/* effect: "coverflow",
+grabCursor:true,
+centeredSlider:true,
+
+coverflowEffect: {
+    rotate:60,
+    stretch:0,
+    depth:100,
+    slideshadows:true,
+},
+slidesPerView:"auto",
+pagination:{
+    el:".swiper-pagination"
+} */
