5f8531e85f07280cf74e065623eb410a96351d1e
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-11-20 11:42:28 +0100

    Add skrollr parameters to titles

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -13,7 +13,7 @@ get_header();
             </div>
         </section>
         <section id="#story" class="story">
-            <h2>L'histoire</h2>
+            <h2> <span data-100="transform: translateY(150px); opacity:0"data-500="transform: translateY(0px); opacity:1">L'histoire</span> </h2>
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
@@ -56,7 +56,9 @@ get_header();
             <?php get_template_part('templates/characters-section'); ?>
             <article id="place">
                 <div>
-                    <h3>Le Lieu</h3>
+                    <h3> <span data-1700="transform: translateY(50px); opacity:0"data-1900="transform: translateY(0px); opacity:1">Le Lieu</span></h3>
+                        <img data-1400="left:1300px; opacity:0" data-top="left:400px; opacity:1" class="big_cloud" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/big_cloud.png'; ?> " alt="Image d'un gros nuage">
+                        <img data-1400="left:1050px; opacity:0" data-top="left:200px; opacity:1" class="little_cloud" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/little_cloud.png'; ?> " alt="Image d'un petit nuage">
                     <p><?php echo get_theme_mod('place'); ?></p>
                 </div>
 
@@ -65,7 +67,7 @@ get_header();
 
 
         <section id="studio">
-            <h2>Studio Koukaki</h2>
+            <h2> <span data-2200="transform: translateY(250px); opacity:0"data-2500="transform: translateY(0px); opacity:1">Studio</span><span data-2250="transform: translateY(250px); opacity:0"data-2550="transform: translateY(0px); opacity:1"> Koukaki</span></h2>
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
--- wp-content/themes/foce-child/templates/oscar-section.php
+++ wp-content/themes/foce-child/templates/oscar-section.php
@@ -1,4 +1,4 @@
 <section class="oscars">
-        <h3> <span>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</span> </h3>
+        <h3> <span data-2700="transform: translateY(250px); opacity:0"data-3100="transform: translateY(0px); opacity:1">Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</span> </h3>
         <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/oscars2022.png'; ?>" alt="Logo des oscars">
 </section>
