abc1031851896deb4b2f5cb6bf3353ac8f924332
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-11-21 11:07:25 +0100

    Add new menu template

---
+++ wp-content/themes/foce-child/templates/menu-burger.php
@@ -0,0 +1,38 @@
+<div id="menu_burger" class="nav_burger">
+  <a id="closeBtn" href="#" class="close">&times;</a>
+  <ul class="burger">
+    <!-- Logo -->
+    <img class="logo_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo_burger.png'; ?> " alt="Logo Fleurs d'oranger & Chats Errants">
+    <!-- Violet orchid -->
+    <img class="orchid_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orchid.png'; ?> " alt="Image d'une orchidé violette">
+    <!-- Blue cat -->
+    <img class="blue_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/blue_cat.png'; ?> " alt="Image d'un chat bleu">
+    <!-- Daisy -->
+    <img class="daisy_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Sunflower.png'; ?> " alt="Image d'une marguerite">
+    <!-- Random flower -->
+    <img class="random_flower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/random_flower_burger.png'; ?> " alt="Image d'une fleur">
+    <!-- Links -->
+    <li class="story_burger title_anim"><a class="lien_burger" href="#story">Histoire</a></li>
+    <li class="title_anim"><a class="lien_burger" href="#characters">Personnages</a></li>
+    <li class="lieu_burger title_anim"><a class="lien_burger" href="#place">Lieu</a></li>
+    <li class="studio_burger title_anim"><a class="lien_burger" href="#studio">Studio Koukaki</a></li>
+    <!-- Orange flower -->
+    <img class="flower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Flower.png'; ?> " alt="Image d'une fleur orange">
+    <!-- Grey cat -->
+    <img class="grey_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/grey_cat.png'; ?> " alt="Image d'un chat gris">
+    <!-- Red cat -->
+    <img class="orange_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orange_cat.png'; ?> " alt="Image d'un chat orange">
+    <!-- Hibiscus flower -->
+    <img class="hibiscus" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Hibiscus.png'; ?> " alt="Image d'une fleur hibiscus">
+    <!-- Koukaki studio image -->
+    <img class="logo_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/studio_koukaki.png'; ?> " alt="Logo Studio Koukaki">
+  </ul>
+</div>
+
+<a href="#" id="openBtn">
+  <span class="burger-icon">
+    <span></span>
+    <span></span>
+    <span></span>
+  </span>
+</a>
