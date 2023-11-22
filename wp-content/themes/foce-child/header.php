2685eeabbb638ea2647239f0a607179449b795a1
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2023-11-22 08:28:08 +0100

    Add responsive to menu

--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -38,11 +38,11 @@
                 <span class="line"></span>
             </button>
             <ul>
-                <!--<li><a href="#story">Histoire</a></li>
-                <li><a href="#characters">Personnages</a></li>-->
+                <li class="menu-mobile"><a href="#story">Histoire</a></li>
+                <li class="menu-mobile"><a href="#characters">Personnages</a></li>
                 <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-                <!--<li><a href="#place">Lieu</a></li>
-                <li><a href="#studio">Studio Koukaki</a></li>-->
+                <li class="menu-mobile"><a href="#place">Lieu</a></li>
+                <li class="menu-mobile"><a href="#studio">Studio Koukaki</a></li>
             </ul>
 
             <?php get_template_part('templates/menu-burger'); ?>
